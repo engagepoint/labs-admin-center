3a8ca86197ef8dbd538900c35cc514b7f4ec5b13
Ievgen Abramov <ievgen.abramov@engagepoint.com>
2014-02-06 04:23:28 +0200

    LAC-1: Code clean jar module

---
+++ AdminCentre-jar/src/main/java/com/engagepoint/university/admincentre/exception/WrongInputArgException.java
@@ -0,0 +1,7 @@
+package com.engagepoint.university.admincentre.exception;
+
+/**
+ * Created by ievgen.abramov on 2/5/14.
+ */
+public class WrongInputArgException {
+}
---
+++ AdminCentre-jar/src/main/test/ConsoleControllerTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+
+public class ConsoleControllerTest {
+    @Test
+    public void testGetCurrentNode() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCurrentNode() throws Exception {
+
+    }
+
+    @Test
+    public void testShowHelp() throws Exception {
+
+    }
+
+    @Test
+    public void testShowVersion() throws Exception {
+
+    }
+
+    @Test
+    public void testDisplayNodes() throws Exception {
+
+    }
+
+    @Test
+    public void testSelectNode() throws Exception {
+
+    }
+
+    @Test
+    public void testChooseParentNode() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateNode() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateKey() throws Exception {
+
+    }
+
+    @Test
+    public void testNameValidation() throws Exception {
+
+    }
+
+    @Test
+    public void testKeyTypeValidation() throws Exception {
+
+    }
+}
