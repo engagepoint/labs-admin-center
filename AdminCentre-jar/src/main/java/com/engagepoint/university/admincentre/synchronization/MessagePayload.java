2e68dccd5645c4e238c0de73122e5fde58a398da
roman.garkavenko <roman.garkavenko@engagepoint.com>
2014-02-06 11:20:26 +0200

    LAC-27: Added forgotten class

---
+++ AdminCentre-jar/src/main/java/com/engagepoint/university/admincentre/synchronization/MessagePayload.java
@@ -0,0 +1,24 @@
+package com.engagepoint.university.admincentre.synchronization;
+
+import com.engagepoint.university.admincentre.entity.AbstractEntity;
+
+public final class MessagePayload {
+	
+	private final CRUDOperation crudOperation;
+	private final AbstractEntity entity;
+	
+	public MessagePayload(CRUDOperation crudOperation,
+								AbstractEntity entity){
+		this.crudOperation = crudOperation;
+		this.entity = entity;
+	}
+	
+	public CRUDOperation getCrudOperation() {
+		return crudOperation;
+	}
+	
+	public AbstractEntity getEntity() {
+		return entity;
+	}
+	
+}
