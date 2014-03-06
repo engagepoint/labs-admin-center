0781f1af11271092264855979c8728ec720a506a
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-06 12:55:39 +0200

    Minor changes.

--- admincentre-web/src/test/java/com/engagepoint/university/admincentre/web/DocumentTest.java
+++ admincentre-web/src/test/java/com/engagepoint/university/admincentre/web/DocumentTest.java
@@ -1,9 +1,5 @@
 package com.engagepoint.university.admincentre.web;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
