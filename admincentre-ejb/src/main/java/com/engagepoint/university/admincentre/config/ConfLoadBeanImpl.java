d7d5d8d979432a7cc2625d1a0dcd47d0c3476cab
leonid.marushevskiy <leonid.marushevskiy@engagepoint.com>
2014-03-07 18:01:10 +0200

    Remove unused classes. Remove Statefull annotation to fix deployment on LTE

--- admincentre-ejb/src/main/java/com/engagepoint/university/admincentre/config/ConfLoadBeanImpl.java
+++ admincentre-ejb/src/main/java/com/engagepoint/university/admincentre/config/ConfLoadBeanImpl.java
@@ -2,9 +2,9 @@ package com.engagepoint.university.admincentre.config;
 
 import com.engagepoint.university.admincentre.util.ConfLoader;
 
-import javax.ejb.Stateful;
+import javax.ejb.Stateless;
 
-@Stateful
+@Stateless
 public class ConfLoadBeanImpl implements ConfLoadBean {
 
     ConfLoader confLoader;
@@ -13,34 +13,42 @@ public class ConfLoadBeanImpl implements ConfLoadBean {
         this.confLoader = new ConfLoader();
     }
 
+    @Override
     public String getChannel() {
         return confLoader.getChannelName();
     }
 
+    @Override
     public void setChannel(String channel) {
         confLoader.setChannelName(channel);
     }
 
+    @Override
     public String getMode() {
         return confLoader.getMode();
     }
 
+    @Override
     public void setMode(String mode) {
         confLoader.setMode(mode);
     }
 
+    @Override
     public String getCluster() {
         return confLoader.getClusterName();
     }
 
+    @Override
     public void setCluster(String cluster) {
         confLoader.setClusterName(cluster);
     }
 
+    @Override
     public String getBasePath() {
         return confLoader.getBasePath();
     }
 
+    @Override
     public void setBasePath(String basePath) {
         confLoader.setBasePath(basePath);
     }
