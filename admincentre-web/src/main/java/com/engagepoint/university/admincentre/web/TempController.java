65af3111ac08bf17a760ae39e3b12148d531ad8a
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-11 12:53:17 +0200

    Sonar issues fixed.

--- admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/TempController.java
+++ admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/TempController.java
@@ -1,41 +1,29 @@
 package com.engagepoint.university.admincentre.web;
 
 import com.engagepoint.university.admincentre.preferences.NodePreferences;
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.prefs.BackingStoreException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @SessionScoped
 public class TempController {
-
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(TempController.class.getName());
 
     public void onExportZip() throws IOException, BackingStoreException {
         File tmpFile = FileController.createTempZip();
         FileController.setPathToTempFile(tmpFile.getPath());
     }
 
-    public void onImportZip(FileUploadEvent event) throws IOException {
+    public void onImportZip(FileUploadEvent event) throws IOException, BackingStoreException {
         UploadedFile file = event.getFile();
         InputStream is = file.getInputstream();
-        try {
-          NodePreferences np = new NodePreferences(null, "");
-            np.importNode(is);
-            np.toString();
-        } catch (BackingStoreException e) {
-           LOGGER.warn("onImportZip: /n ", e);
-        } catch (IOException ioException) {
-            LOGGER.warn("onImportZip: message ", ioException);
-        }
+        NodePreferences np = new NodePreferences(null, "");
+        np.importNode(is);
+        np.toString();
     }
 }
