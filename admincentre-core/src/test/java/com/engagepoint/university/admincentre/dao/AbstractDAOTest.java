8cce380b1b9da9c239c9d9d319cb084a6a7a3906
kalexandru <oleksandr.kushnir@engagepoint.com>
2014-03-14 11:54:36 +0200

    LAC-68 commented test

--- admincentre-core/src/test/java/com/engagepoint/university/admincentre/dao/AbstractDAOTest.java
+++ admincentre-core/src/test/java/com/engagepoint/university/admincentre/dao/AbstractDAOTest.java
@@ -1,35 +1,28 @@
 package com.engagepoint.university.admincentre.dao;
 
-import com.engagepoint.university.admincentre.entity.Key;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-
 /**
  * Created by oleksandr.kushnir on 3/13/14.
  */
 public class AbstractDAOTest {
-
-
-    GenericDAO<Key> genericDAO;
-
-    @Before
-    public void setup(){
-        AbstractDAO<Key> tAbstractDAO = (AbstractDAO<Key>) mock(GenericDAO.class);
-
-            //when(tAbstractDAO.read("13")).thenReturn("13");
-
-
-    }
-
-    @Test
-    public Key testRead(){
-
-       // Assert.assertEquals("13",);
-        Key key = null;
-        return key;
-    }
+//
+//    GenericDAO<Key> genericDAO;
+//
+//    @Before
+//    public void setup(){
+//        AbstractDAO<Key> tAbstractDAO = (AbstractDAO<Key>) mock(GenericDAO.class);
+//
+//            //when(tAbstractDAO.read("13")).thenReturn("13");
+//
+//
+//    }
+//
+//    @Test
+//    public Key testRead(){
+//
+//       // Assert.assertEquals("13",);
+//        Key key = null;
+//        return key;
+//    }
 
 
 }
