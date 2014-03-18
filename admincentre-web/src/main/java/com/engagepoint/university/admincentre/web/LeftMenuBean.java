4ae4f42ea59e4bad11566f6b130b5424abcb88bc
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-18 16:34:54 +0200

    Some Sonar critical & major bug fixes.

--- admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/LeftMenuBean.java
+++ admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/LeftMenuBean.java
@@ -1,22 +1,16 @@
 package com.engagepoint.university.admincentre.web;
 
+import com.engagepoint.component.menu.UIMenuItem;
+import com.engagepoint.component.menu.model.DefaultMenuModel;
+import com.engagepoint.component.menu.model.MenuModel;
+import com.engagepoint.university.admincentre.datatransfer.DataBean;
+import com.engagepoint.university.admincentre.synch.Synch;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.ActionEvent;
-import javax.faces.event.ActionListener;
-
-import com.engagepoint.component.menu.UIMenuItem;
-import com.engagepoint.component.menu.UIMenuSection;
-import com.engagepoint.component.menu.model.DefaultMenuModel;
-import com.engagepoint.component.menu.model.MenuModel;
-import com.engagepoint.university.admincentre.datatransfer.DataBean;
-import com.engagepoint.university.admincentre.exception.SynchronizationException;
-import com.engagepoint.university.admincentre.synch.Synch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,57 +18,62 @@ import org.slf4j.LoggerFactory;
 @RequestScoped
 public class LeftMenuBean {
 
-	private MenuModel model;
     public static final String EDITOR_URL = "/editor.xhtml";
     public static final String SETTINGS_URL = "/settings.xhtml";
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataBean.class.getName());
+    @EJB
+    private Synch synch;
+    private String clusterName;
+    private MenuModel model;
 
-	@PostConstruct
-	 public void init() {
+    @PostConstruct
+    public void init() {
 
-		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
-	    String viewId = viewRoot.getViewId();
-		
-	    if(viewId.endsWith(EDITOR_URL)){
-	    	editorMenu();
-	    }else if(viewId.endsWith(SETTINGS_URL)){
-	    	settingsMenu();
-	    }
+        UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+        String viewId = viewRoot.getViewId();
 
-	}
+        if (viewId.endsWith(EDITOR_URL)) {
+            editorMenu();
+        } else if (viewId.endsWith(SETTINGS_URL)) {
+            settingsMenu();
+        }
+    }
 
-	private void editorMenu(){
-		model = new DefaultMenuModel();
+    public String getInfo() {
+        if (synch.isConnected()) {
+            return "Connected. Channel name: " + synch.getChannelName()
+                    + "; Cluster name: " + synch.getClusterName();
+        } else {
+            return "Disconnected.";
+        }
+    }
 
-		// menuItem creation
+    private void editorMenu() {
+        model = new DefaultMenuModel();
+        UIMenuItem item = new UIMenuItem();
+        item.setId("view_table");
+        item.setValue("View_table");
+        item.setUrl("/pages/editor.xhtml");
+        item.setTitle("View table");
+        model.addMenuItem(item);
+    }
 
-		UIMenuItem item = new UIMenuItem();
-		item.setId("view_table");
-		item.setValue("View_table");
-		item.setUrl("/pages/editor.xhtml");
-		item.setTitle("View table");
-
-		model.addMenuItem(item);
-
-	}
-	
-	private void settingsMenu(){
-		model = new DefaultMenuModel();
-		// menuItem creation
-
-		UIMenuItem item = new UIMenuItem();
+    private void settingsMenu() {
+        model = new DefaultMenuModel();
+        UIMenuItem item = new UIMenuItem();
         item.setId("import_export");
         item.setValue("Import_export");
         item.setUrl("/pages/settings.xhtml");
         item.setTitle("Import/Export");
         item.setActiveLinkSelection(true);
         model.addMenuItem(item);
-	}
-	
-	public MenuModel getModel() {
-		return model;
-	}
+    }
 
-	public void setModel(MenuModel model) {
-		this.model = model;
-	}
+    public MenuModel getModel() {
+        return model;
+    }
+
+    public void setModel(MenuModel model) {
+        this.model = model;
+    }
 }
