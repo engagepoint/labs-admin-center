9d8d29264de40215bda16db66ff017d9f95604d7
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-18 16:33:58 +0200

    Some Sonar critical & major bug fixes.

--- admincentre-core/src/main/java/com/engagepoint/university/admincentre/entity/Key.java
+++ admincentre-core/src/main/java/com/engagepoint/university/admincentre/entity/Key.java
@@ -2,24 +2,18 @@ package com.engagepoint.university.admincentre.entity;
 
 import org.hibernate.search.annotations.Indexed;
 
-
 @Indexed
 public class Key extends AbstractEntity {
 
     private static final long serialVersionUID = -5430748795395302687L;
-
     private KeyType type;
-
     private String value;
 
-
     public Key() {
-
     }
 
     public Key(String parentNodeId, String name, KeyType type, String value) {
         super(name, parentNodeId);
-
         this.type = type;
         this.value = value;
     }
@@ -40,28 +34,28 @@ public class Key extends AbstractEntity {
         this.value = value;
     }
 
-
-
     @Override
     public boolean equals(Object obj) {
-    	if (this == obj)
-			return true;
-		if ( !(obj instanceof Key) )
-			return false;
-		Key key = (Key) obj;
-		return super.getId().equals(key.getId())
-				&& type == key.type
-				&& value.equals(key.value);
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Key)) {
+            return false;
+        }
+        Key key = (Key) obj;
+        return super.getId().equals(key.getId())
+                && type == key.type
+                && value.equals(key.value);
     }
-    
+
     @Override
     public int hashCode() {
-    	return (super.getId() + type.toString() + value).hashCode();
+        return (super.getId() + type.toString() + value).hashCode();
     }
     
-	@Override
-	public String toString() {	// do not delete!
-		return super.getId() + " " + type + " " + value;
-	}
-
+    /* do not delete!*/
+    @Override
+    public String toString() {
+        return super.getId() + " " + type + " " + value;
+    }
 }
