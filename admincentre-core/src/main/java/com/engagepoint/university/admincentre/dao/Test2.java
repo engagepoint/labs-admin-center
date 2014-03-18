7f7b5c8a01f0e6e5297afa603e7cfc8b86a0cff7
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-18 16:33:42 +0200

    Some Sonar critical & major bug fixes.

--- admincentre-core/src/main/java/com/engagepoint/university/admincentre/dao/Test2.java
+++ admincentre-core/src/main/java/com/engagepoint/university/admincentre/dao/Test2.java
@@ -6,39 +6,31 @@ import java.util.List;
 import com.engagepoint.university.admincentre.entity.Key;
 
 public class Test2 {
-    public static void main(String[] args) {
 
-        // Preferences preferences = new NodePreferences(null, "");
-        // Preferences another = preferences.node("/newNode/Node1/Node1.1");
-        // Preferences another1 = preferences.node("/newNode/Node1/Node1.2");
-        // Preferences another2 = preferences.node("/newNode/Node1");
-        //
-        // another.put("11", "11");
-        // another1.put("12", "12");
-        // another2.put("13", "13");
+    public static void main(String[] args) throws IOException {
+/*
+         Preferences preferences = new NodePreferences(null, "");
+         Preferences another = preferences.node("/newNode/Node1/Node1.1");
+         Preferences another1 = preferences.node("/newNode/Node1/Node1.2");
+         Preferences another2 = preferences.node("/newNode/Node1");
+        
+         another.put("11", "11");
+         another1.put("12", "12");
+         another2.put("13", "13");
 
-        // try {
-        // another2.removeNode();
-        // } catch (BackingStoreException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
+         try {
+         another2.removeNode();
+         } catch (BackingStoreException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+         }
 
-//        another1.put("14", "14");
-//        another2.put("15", "15");
-//        another1.put("16", "16");
-//        another2.put("17", "17");
-
+        another1.put("14", "14");
+        another2.put("15", "15");
+        another1.put("16", "16");
+        another2.put("17", "17");
+*/
         KeyDAO keyDAO = KeyDAO.getInstance();
-
-        try {
-            List<Key> list = keyDAO.search("11");
-            System.out.println(list.get(0).getName() + "\n" + list.get(0).getValue());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+        List<Key> list = keyDAO.search("11");
     }
-
 }
