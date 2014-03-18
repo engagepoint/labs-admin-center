1d1a72349a854d91bfe65cd4054ea21ffa41d479
Artem Lysenko <artem.lysenko@engagepoint.com>
2014-03-18 11:14:28 +0200

    Major Sonar issues fixes

--- admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/LeftMenuBean.java
+++ admincentre-web/src/main/java/com/engagepoint/university/admincentre/web/LeftMenuBean.java
@@ -14,120 +14,104 @@ import com.engagepoint.component.menu.UIMenuItem;
 import com.engagepoint.component.menu.UIMenuSection;
 import com.engagepoint.component.menu.model.DefaultMenuModel;
 import com.engagepoint.component.menu.model.MenuModel;
+import com.engagepoint.university.admincentre.datatransfer.DataBean;
 import com.engagepoint.university.admincentre.exception.SynchronizationException;
 import com.engagepoint.university.admincentre.synch.Synch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ManagedBean(name = "leftMenu")
 @RequestScoped
 public class LeftMenuBean {
-	
-	@EJB
-	private Synch synch;
-	
-	private String clusterName;
-	
-	public String getClusterName() {
-		return clusterName;
-	}
-	
-	public void setClusterName(String clusterName) {
-		try {
-			synch.autoConnect(clusterName);
-			this.clusterName = clusterName;
-		} catch (IllegalStateException e) {
-		} catch (SynchronizationException e) {
-		}
-	}
-	
-	public String getInfo(){
-		if(synch.isConnected()){
-			return "Connected. Channel name: " + synch.getChannelName()
-					+ "; Cluster name: " + synch.getClusterName();
-		}else{
-			return "Disconnected.";
-		}
-	}
-	
-	private MenuModel model;
 
-	@PostConstruct
-	 public void init() {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataBean.class.getName());
+    @EJB
+    private Synch synch;
+    private String clusterName;
 
-		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
-	    String viewId = viewRoot.getViewId();
-		
-	    if(viewId.endsWith("/editor.xhtml")){
-	    	editorMenu();
-	    }else if(viewId.endsWith("/settings.xhtml")){
-	    	settingsMenu();
-	    }else if(viewId.endsWith("/import_export.xhtml")){
-	    	settingsMenu();
-	    }
+    public String getClusterName() {
+        return clusterName;
+    }
 
-	}
+    public void setClusterName(String clusterName) {
+        try {
+            synch.autoConnect(clusterName);
+            this.clusterName = clusterName;
+        } catch (IllegalStateException e) {
+            LOGGER.warn("Failed to set cluster name " + clusterName, e);
+        } catch (SynchronizationException ex) {
+            LOGGER.warn("Failed to syncronize with cluster name " + clusterName, ex);
+        }
+    }
 
-	private void editorMenu(){
-		model = new DefaultMenuModel();
+    public String getInfo() {
+        if (synch.isConnected()) {
+            return "Connected. Channel name: " + synch.getChannelName()
+                    + "; Cluster name: " + synch.getClusterName();
+        } else {
+            return "Disconnected.";
+        }
+    }
+    private MenuModel model;
 
-		// menuItem creation
+    @PostConstruct
+    public void init() {
 
-		UIMenuItem item = new UIMenuItem();
-		item.setId("view_table");
-		item.setValue("View_table");
-		item.setUrl("#");
-		item.setTitle("View table");
+        UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+        String viewId = viewRoot.getViewId();
 
-		model.addMenuItem(item);
+        if (viewId.endsWith("/editor.xhtml")) {
+            editorMenu();
+        } else if (viewId.endsWith("/settings.xhtml")) {
+            settingsMenu();
+        } else if (viewId.endsWith("/import_export.xhtml")) {
+            settingsMenu();
+        }
+    }
 
-		
-		item = new UIMenuItem();
-		item.setId("search");
-		item.setValue("Search");
-		item.setUrl("#");
-		item.setTitle("Search");
-		
-		model.addMenuItem(item);
-	}
-	
-	private void settingsMenu(){
-		model = new DefaultMenuModel();
+    /**
+     * Menu creation
+     */
+    private void editorMenu() {
+        model = new DefaultMenuModel();
+        UIMenuItem item = new UIMenuItem();
+        item.setId("view_table");
+        item.setValue("View_table");
+        item.setUrl("#");
+        item.setTitle("View table");
+        model.addMenuItem(item);
+        item = new UIMenuItem();
+        item.setId("search");
+        item.setValue("Search");
+        item.setUrl("#");
+        item.setTitle("Search");
+        model.addMenuItem(item);
+    }
 
-		// menuItem creation
+    /**
+     * Menu item "Synch" creation
+     */
+    private void settingsMenu() {
+        model = new DefaultMenuModel();
+        UIMenuItem item = new UIMenuItem();
+        item.setId("synch");
+        item.setValue("Synch");
+        item.setUrl("/pages/settings.xhtml");
+        item.setTitle("Synchronization");
+        model.addMenuItem(item);
+        item = new UIMenuItem();
+        item.setId("import_export");
+        item.setValue("Import_export");
+        item.setUrl("/pages/import_export.xhtml");
+        item.setTitle("Import/Export");
+        model.addMenuItem(item);
+    }
 
-		UIMenuItem item = new UIMenuItem();
-		item.setId("synch");
-		item.setValue("Synch");
-		item.setUrl("/pages/settings.xhtml");
-		item.setTitle("Synchronization");
+    public MenuModel getModel() {
+        return model;
+    }
 
-		model.addMenuItem(item);
-
-		
-		item = new UIMenuItem();
-		item.setId("import_export");
-		item.setValue("Import_export");
-		item.setUrl("/pages/import_export.xhtml");
-		item.setTitle("Import/Export");
-		
-		model.addMenuItem(item);
-                
-	}
-	
-	public MenuModel getModel() {
-		return model;
-	}
-
-	public void setModel(MenuModel model) {
-		this.model = model;
-	}
-	
-//	public String autoConnect(){
-//		try {
-//			return "leftMenu";
-//		} catch (IllegalStateException e) {
-//			return "";
-//		} catch (SynchronizationException e) {
-//			return "";
-//		}
-//	}
+    public void setModel(MenuModel model) {
+        this.model = model;
+    }
 }
